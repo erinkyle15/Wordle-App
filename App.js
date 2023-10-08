b2ca596a92a12c2b5736a17ecc0dcb2b13d15d1d
erinkyle15 <erinkyle15@gmail.com>
2023-10-08 15:04:29 +0100

    inital commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { colors } from './src/constants';
+import Keyboard from './src/components/Keyboard/Keyboard';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+
+      <Text style={styles.title}>ERIN'S WORDLE</Text>
+
+      <View style={styles.map}>
+        <View style={styles.row}>
+          <View style={styles.cell} />
+          <View style={styles.cell} />
+          <View style={styles.cell} />
+          <View style={styles.cell} />
+          <View style={styles.cell} />
+          <View style={styles.cell} />
+        </View>
+      </View>
+
+      <Keyboard />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+    alignItems: 'center',
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
+  },
+  map: {
+    backgroundColor: "red",
+    alignSelf: 'stretch',
+
+    height: 100,
+  },
+  row: {
+    backgroundColor: "blue",
+    alignSelf: 'stretch',
+    height: 50,
+  },
+  cell: {
+    backgroundColor: "green",
+    width: 30,
+    height: 30,
+  },
+});
